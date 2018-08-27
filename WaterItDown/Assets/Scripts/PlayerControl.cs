7888c290a98582695c43d074ebff2c91b1610cf0
Jari Jokinen <tredic@gmail.com>
2018-08-27 13:32:11 +0300

    deleted shooting scripts

--- WaterItDown/Assets/Scripts/PlayerControl.cs
+++ WaterItDown/Assets/Scripts/PlayerControl.cs
@@ -12,29 +12,15 @@ public class PlayerControl : MonoBehaviour {
     public float speed;
     public float health = 5;
 
-    [Header("Bullets")]
-    public GameObject Bullet;
-    public Transform AmmoPool;
-    private float fireRateTimer;
-
     private Vector3 pos;
 
 
     void Start () {
         rb = GetComponent<Rigidbody>();
-        op = new ObjectPooler(CreateBulletObject, AmmoPool, 20, false);
     }
-
-    public GameObject CreateBulletObject()
-    {
-        GameObject obj = Instantiate(Bullet);
-        return obj;
-    }
-
 
     void FixedUpdate () {
         MovePlayer();
-        //Shoot();
     }
 
     private void MovePlayer()
@@ -45,30 +31,6 @@ public class PlayerControl : MonoBehaviour {
             
         }
         rb.MovePosition(Vector3.Lerp(transform.position, pos, Time.deltaTime * speed));
-    }
-
-    private void Shoot()
-    {
-        if (Input.GetButton("Fire1") && Time.time > fireRateTimer)
-        {
-            GameObject bullet = op.GetObject();
-            
-            if (bullet == null)
-                return;
-            else
-            {
-                bullet.transform.position = this.transform.position;
-                bullet.SetActive(true);
-                bullet.GetComponent<Rigidbody>().velocity = Vector2.up * bullet.GetComponent<Bullet>().BulletSpeed;
-                StartCoroutine(BulletLifeTime(bullet));
-            }
-            fireRateTimer = Time.time + Bullet.GetComponent<Bullet>().FireRate;
-        }
-    }
-    IEnumerator BulletLifeTime(GameObject bullet)
-    {
-        yield return new WaitForSeconds(3);
-        bullet.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
